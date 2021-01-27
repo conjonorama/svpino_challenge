267df41c270af0165e426f8119f805acabacdcaf
conjonorama <connor.r.brown@outlook.com>
2021-01-26 20:46:17 -0800

    Some Twitter submissions have been added

--- challenge_2.py
+++ challenge_2.py
@@ -23,4 +23,21 @@ print(t)
 
 print(find_missing(t))
 
+# Submission 1 from twitter (Which is so simple and funny)
+# If it's always from 1 to 100 it's pretty simple - in Python 3:
+# https://twitter.com/RomuloF27/status/1354060816908226565?s=20
 
+def findMissingNumber(arr):
+    return 5050 - sum(arr)
+
+# Sumbmission2 from Twitter
+# I have no idea what is going on in this code - getting there though :)
+# https://twitter.com/shraddha_pooja/status/1354136657927270401?s=20
+def findMissingNumber1(arr):
+    arr_len = len(arr)
+    n = arr_len + 1
+    missing_num = (n>>1)&1 ^ (1 if ((n&1)>0) else n) #XOR 1 to n
+    for _ in range(arr_len): missing_num ^= arr[_]
+    return missing_num
+
+print(findMissingNumber1([1,4,3,5,7,6,8,2,11,9]))
