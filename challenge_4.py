636e137cba370d2174f23d38d7f134ffb1a1ef0a
conjonorama <connor.r.brown@outlook.com>
2021-02-02 19:19:21 -0800

    Challenge 4 - my submissions usings dicts + lists

---
+++ challenge_4.py
@@ -0,0 +1,31 @@
+# Write a function that removes every duplicate value in an array.
+
+# There could be more than one value duplicated. You should remove all of them leaving a single copy of the value.
+
+# My attempt 1 - using list
+def findUnique(arr):
+    t = []
+    for i in arr:
+        if i not in t:
+            t.append(i)
+    return(t)
+
+# My attempt 2 - using dictionary and taking keys out
+def findUnique2(arr):
+    d = {}
+    for i in arr:
+        d[i] = d.get(i, 0) + 1
+
+    t = []
+    for k in d.keys():
+        t.append(k)
+
+    return t
+    # return d.keys()
+
+
+
+if __name__ == '__main__':
+    array = 'a b b c d c d e f f g h'.split()
+    print(findUnique(array))
+    print(findUnique2(array))
